e6f5ef70f6c1c14db574401a5e0277d4390d4685
Joshua Tjhin <xuheng.tjhin@gmail.com>
2020-03-16 22:45:16 -0700

    Color file icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,24 +1,29 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const SVG = styled.svg`
+  color: rgba(3, 47, 98, 0.5);
+  fill: currentColor;
+`;
 
 export const FolderIcon = () => (
-  <svg
+  <SVG
     aria-label="directory"
     viewBox="0 0 14 16"
     version="1.1"
     width="14"
     height="16"
     role="img"
-    style={{ color: "rgba(3,47,98,.5)" }}
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M13 4H7V3c0-.66-.31-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zM6 4H1V3h5v1z"
     ></path>
-  </svg>
+  </SVG>
 );
 
 export const FileIcon = () => (
-  <svg
+  <SVG
     aria-label="file"
     viewBox="0 0 12 16"
     version="1.1"
@@ -27,8 +32,8 @@ export const FileIcon = () => (
     role="img"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"
     ></path>
-  </svg>
+  </SVG>
 );
